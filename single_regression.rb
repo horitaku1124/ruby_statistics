07c3cc45c5434e3ae37f115bf578a2c3761df834
Horimatsu Takuya <hori@ict-start.com>
2013-11-10 16:55:00 +0900

    単回帰式の追加

---
+++ single_regression.rb
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+require './MyArray'
+
+x = [10,4,2,2,8,9,7,5,1,3,4,6,8,11,6]
+y = [10,5,5,4,4,6,6,2,1,2,7,7,9,8,4]
+
+a = x.devmulOf(y) / x.devsq
+b = y.mean - a * x.mean
+
+print "a=",a,"\n"
+print "b=",b,"\n"
+
+puts "近似直線"
+
+print "Y = ", a, "X + ", b
